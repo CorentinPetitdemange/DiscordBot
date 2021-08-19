a6ef49643453956a9b0b6f01c30c58bcba539990
Corentin Petitdemange <corentinpetitdemange@gmail.com>
2021-08-19 23:10:52 +0200

    test

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os 
 
 load_dotenv(dotenv_path="config")
-client = discord.Client()
+"""client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -16,7 +16,15 @@ async def on_message(message):
     if message.content.lower() =="test":
         await message.channel.send("ok", delete_after=5)
 
-    #print(message.content)
+    #print(message.content)"""
+
+class GitSpeak(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix="/")
+
+    async def on_ready(self):
+        print(f"{self.user.display_name} est connecté au serveur.")
 
 
-client.run(os.getenv("TOKEN"))
+Git_speak = GitSpeak()
+Git_speak.run(os.getenv("TOKEN"))
